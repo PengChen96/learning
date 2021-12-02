5f6586644bf8c5aca2806767f4003b5201c30e43
pengchen96 <2601349541@qq.com>
2021-12-02 21:05:08 +0800

    feat: tree-shaking

--- open-source/output/eg.tree-shaking.basic.output.js
+++ open-source/output/eg.tree-shaking.basic.output.js
@@ -1,15 +1,1 @@
-(() => {
-  "use strict";
-  var e = {};
-  (e => {
-    "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {value: "Module"}), Object.defineProperty(e, "__esModule", {value: !0})
-  })(e);
-  var o = {
-    d: (e, t) => {
-      for (var r in t) o.o(t, r) && !o.o(e, r) && Object.defineProperty(e, r, {enumerable: !0, get: t[r]})
-    }, o: (e, o) => Object.prototype.hasOwnProperty.call(e, o)
-  }, t = {};
-  o.d(t, {v: () => r, d: () => l});
-  const r = (e, o) => (console.log("hi, sumFun!"), e + o), l = (e, o) => e - o;
-  (0, t.v)(1, 2), module.exports = e
-})();
+(()=>{"use strict";var e={};(e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})})(e);var o={d:(e,t)=>{for(var r in t)o.o(t,r)&&!o.o(e,r)&&Object.defineProperty(e,r,{enumerable:!0,get:t[r]})},o:(e,o)=>Object.prototype.hasOwnProperty.call(e,o)},t={};o.d(t,{v:()=>r,d:()=>l});const r=(e,o)=>(console.log("hi, sumFun!"),e+o),l=(e,o)=>e-o;(0,t.v)(1,2),module.exports=e})();
--- open-source/tree-shaking.basic.js
+++ open-source/tree-shaking.basic.js
@@ -6,9 +6,10 @@ const fun = () => {
 fun();
 
 /**
- * tree shaking
+ * tree shaking: 基于ES2015模块语法的静态结构特性，最早是由rollup提出的概念
+ * 配置：
  * 1. 需要webpack.config.js配置  optimization: {usedExports: true,}
- * 2. 需要package.json开启"sideEffects": false,
+ * 2. 需要package.json开启"sideEffects": false, 可以通过设置["*.css"],跳过整个模块/文件和整个文件子树,不进行移除
  *
  * 注意： 1. 只有production会真的删除无用代码
  *       2. 有console.log就不会移除
